f31745335efb28cbe46a477ba76369705d085781
BrKGd <gleidsonfigueira@gmail.com>
2026-02-24 14:58:58 -0300

    Modify Vite config for GitHub Pages
    Updated Vite configuration for GitHub Pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/diario_de_bordo/'
+
+  // IMPORTANTE para GitHub Pages
+  base: '/diario_de_bordo/',
+
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
+  }
 })
